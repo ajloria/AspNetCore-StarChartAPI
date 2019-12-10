54bafaf5d137b26c0e6a388c3c4c5bfa7248754d
Andrew Joshua Loria <ajloria@gmail.com>
2019-12-10 09:34:24 -0800

    Update CelestialObject.cs

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -17,6 +17,7 @@ namespace StarChart.Models
         public string Name { get; set; }
 
         public int? OrbitedObjectId { get; set; }
+        [NotMapped]
         public List<CelestialObject> Satellites { get; set; }   
         public TimeSpan OrbitalPeriod { get; set; }
 
